2dc84adc8b6f6e6d4f7ab80147e203636f094cc5
devjunior30 <edyjunior299@gmail.com>
2023-08-27 19:03:06 -0300

    topico de milisegundos adicionados e finalização na parte do script

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 
 // let watch = document.querySelector('.watch'
 
+let milliseconds = 0
 let seg = 0
 let min = 0
 let hrs = 0
@@ -20,7 +21,10 @@ function digits(number) {
 
 function start() {
     cron = setInterval( function() {
+        milliseconds ++
+        if(milliseconds == 100) {
         seg++ 
+        milliseconds = 0
         if(seg == 60) {
             min++
             seg = 0
@@ -29,8 +33,9 @@ function start() {
                 min = 0
             }
         }
-        document.querySelector('.watch').innerHTML = digits(hrs)+':'+digits(min)+':'+digits(seg)
-    },100)
+    }
+        document.querySelector('.watch').innerHTML = digits(hrs)+':'+digits(min)+':'+digits(seg)+':'+digits(milliseconds)
+    },10)
 }
 
 function pause() {
@@ -39,6 +44,7 @@ function pause() {
 
 function stop() {
     clearInterval(cron)
+    milliseconds = 0
     seg = 0
     min = 0
     hrs =0 
